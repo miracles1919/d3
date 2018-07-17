4560de3c2a9bdd0716ab8d4828867665a89572a2
miracles <miracles@fanfandata.com>
2018-07-17 23:05:19 +0800

    增加可移动的饼图

--- src/page/qingdao.js
+++ src/page/qingdao.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { render } from 'react-dom'
-import * as d3Geo from 'd3-geo'
-import * as d3Sel from 'd3-selection'
 import qdJSON from '../json/370200.json'
 
 import style from './qingdao.less'
 
-console.log(qdJSON)
-
-const d3 = Object.assign({}, d3Geo, d3Sel, require('d3-scale'))
+const d3 = Object.assign({},
+  require('d3-geo'),
+  require('d3-selection'),
+  require('d3-scale'),
+  require('d3-shape'),
+  require('d3-scale-chromatic'),
+  require('d3-transition'),
+)
 
 class DataV extends React.Component {
 
@@ -19,7 +22,7 @@ class DataV extends React.Component {
     const projection = d3.geoMercator().center([120.4651, 36.3373]).scale(10000).translate([width / 1.7, height / 2])
     const path = d3.geoPath(projection)
     const colorArr = ['#7CDFD0', '#FE8F8C', '#BC8CEF', '#13AEEB', '#FDCB52', '#ADF3F8', '#E3C9FE', '#7CDFD0',
-    '#FFECBD', '#F7B7FF', '#5BB9AB', '#FFD3D2']
+      '#FFECBD', '#F7B7FF', '#5BB9AB', '#FFD3D2']
     const nameArr = qdJSON.features.map(item => item.properties.name)
 
     this.width = width
@@ -77,6 +80,75 @@ class DataV extends React.Component {
       .attr("y", d => d.y)
       .style('fill', (d, i) => colorArr[i])
 
+
+    const innerRadius = 100, outerRadius = 150, padAngle = 0.01
+    const pieArr = [5, 7, 19, 4, 10]
+    let pieData = d3.pie()(pieArr)
+    const arc = d3.arc().innerRadius(innerRadius).outerRadius(outerRadius).padAngle(padAngle)
+    let width = 600, height = 600
+
+    console.log('pieData', pieData)
+
+    const pieSvg = d3.select('#pie')
+      .attr('width', width)
+      .attr('height', height)
+      .selectAll('g')
+      .data(pieData)
+      .enter()
+      .append('g')
+      .attr('transform', `translate(${width / 2}, ${height / 2})`)
+
+    pieSvg
+      .append('path')
+      .style('fill', (d, i) => d3.schemeCategory10[i])
+
+      .attr('d', d => arc(d))
+      .on('mouseover', function () {
+
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('transform', d => {
+            let offset = arc.centroid(d).map(item => item/8)
+            return `translate(${offset})`
+          })
+      })
+      .on('mouseout', function(){
+        d3.select(this)
+          .transition()
+          .duration(500)
+          .attr('transform', 'translate(0, 0)' )
+      })
+
+    pieSvg
+      .append('text')
+      .attr('transform', d => `translate(${arc.centroid(d)})`)
+      .attr('text-anchor', 'middle')
+      .text(d => d.data)
+
+    // .on("mouseover", function (d, i) {
+    //   d3.select(this)
+    //     .transition()
+    //     .duration(2000)
+    //     .delay(100)
+    //     .ease("bounce")
+    //     .attr("transform", function (d) {
+    //       alert(arc.centroid(d));
+    //       return "translate(" + arc.centroid(d) + ")";
+    //     });
+    // })
+    // .on("mouseout", function (d, i) {
+    //   d3.select(this)
+    //     .transition()
+    //     .duration(2000)
+    //     //.delay(10000)
+    //     .attr("transform", function (d) {
+    //       //alert(arc.centroid(d));
+    //       return "translate(" + "-" + arc.centroid(d) + ")";//centroid返回的是弧形的重心与弧心的相对位置
+    //     });
+    // })
+
+
   }
 
 
@@ -85,17 +157,20 @@ class DataV extends React.Component {
     const { colorArr, nameArr } = this
     console.log(nameArr)
     return (
-      <div className={style.container}>
-        <div className={style.legend}>
-          <ul>
-            {nameArr.map((item, index) => {
-              return (
-                <li key={index}><i style={{ backgroundColor: colorArr[index] }}/><span>{item}</span></li>
-              )
-            })}
-          </ul>
+      <div>
+        <div className={style.container}>
+          <div className={style.legend}>
+            <ul>
+              {nameArr.map((item, index) => {
+                return (
+                  <li key={index}><i style={{ backgroundColor: colorArr[index] }} /><span>{item}</span></li>
+                )
+              })}
+            </ul>
+          </div>
+          <div id="svg"></div>
         </div>
-        <div id='svg'></div>
+        <svg id="pie"></svg>
       </div>
     )
   }
