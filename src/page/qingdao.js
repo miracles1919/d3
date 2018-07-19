b1e64a99987b3fc5f1b80ede0358f2fe34ce0f36
miracles <miracles@fanfandata.com>
2018-07-19 21:01:21 +0800

    增加柱状图

--- src/page/qingdao.js
+++ src/page/qingdao.js
@@ -11,6 +11,8 @@ const d3 = Object.assign({},
   require('d3-shape'),
   require('d3-scale-chromatic'),
   require('d3-transition'),
+  require('d3-array'),
+  require('d3-axis')
 )
 
 class DataV extends React.Component {
@@ -34,13 +36,20 @@ class DataV extends React.Component {
 
   componentDidMount() {
 
+    this.renderMap()
+
+    this.renderPie()
+
+    this.renderBar()
+  }
+
+  renderMap() {
     let arr = [], centers = []
     const { path, colorArr } = this
 
     for (let i = 0; i < 12; i++) {
       arr.push(Math.floor(Math.random() * 100))
     }
-
 
     console.log(arr)
 
@@ -66,9 +75,6 @@ class DataV extends React.Component {
         centers.push({ x, y })
       })
 
-    console.log(centers)
-
-
     svg.selectAll('rect')
       .data(centers)
       .enter()
@@ -79,8 +85,9 @@ class DataV extends React.Component {
       .attr("x", d => d.x)
       .attr("y", d => d.y)
       .style('fill', (d, i) => colorArr[i])
+  }
 
-
+  renderPie() {
     const innerRadius = 100, outerRadius = 150, padAngle = 0.01
     const pieArr = [5, 7, 19, 4, 10]
     let pieData = d3.pie()(pieArr)
@@ -109,15 +116,15 @@ class DataV extends React.Component {
           .transition()
           .duration(500)
           .attr('transform', d => {
-            let offset = arc.centroid(d).map(item => item/8)
+            let offset = arc.centroid(d).map(item => item / 8)
             return `translate(${offset})`
           })
       })
-      .on('mouseout', function(){
+      .on('mouseout', function () {
         d3.select(this)
           .transition()
           .duration(500)
-          .attr('transform', 'translate(0, 0)' )
+          .attr('transform', 'translate(0, 0)')
       })
 
     pieSvg
@@ -126,27 +133,66 @@ class DataV extends React.Component {
       .attr('text-anchor', 'middle')
       .text(d => d.data)
 
-    // .on("mouseover", function (d, i) {
-    //   d3.select(this)
-    //     .transition()
-    //     .duration(2000)
-    //     .delay(100)
-    //     .ease("bounce")
-    //     .attr("transform", function (d) {
-    //       alert(arc.centroid(d));
-    //       return "translate(" + arc.centroid(d) + ")";
-    //     });
-    // })
-    // .on("mouseout", function (d, i) {
-    //   d3.select(this)
-    //     .transition()
-    //     .duration(2000)
-    //     //.delay(10000)
-    //     .attr("transform", function (d) {
-    //       //alert(arc.centroid(d));
-    //       return "translate(" + "-" + arc.centroid(d) + ")";//centroid返回的是弧形的重心与弧心的相对位置
-    //     });
-    // })
+  }
+
+  renderBar() {
+    let data = [10, 20, 30, 40, 33, 24, 12, 5]
+    let width = 400, height = 400, padding = { left: 30, right: 30, top: 20, bottom: 20 }
+    let xArr = ['10-20', '20-30', '30-40', '40-50', '50-60', '60-70', '70-80', '80-90']
+    let
+      xScale = d3.scaleLinear().domain([0, data.length]).range([0, width - padding.left - padding.right]),
+      xTxtScale = d3.scaleBand().domain(xArr).range([0, width - padding.left - padding.right]),
+      yScale = d3.scaleLinear().domain([0, d3.max(data)]).range([height - padding.top - padding.bottom, 0]),
+      xAxis = d3.axisBottom(xTxtScale),
+      yAxis = d3.axisLeft(yScale)
+
+    let svg = d3.select('#bar')
+      .attr('width', width)
+      .attr('height', height)
+
+    let y = svg.append('g')
+    .attr('transform', `translate(${padding.left}, ${padding.top})`)
+    .call(yAxis)
+
+    y.select('path')
+    .attr('stroke', '#E8E8E8')
+
+    y.selectAll('line')
+    .attr('stroke', '#E8E8E8')
+    .attr('x2', d => width - padding.left - padding.right)
+
+    svg.selectAll('rect')
+      .data(data)
+      .enter()
+      .append('rect')
+      // .attr('class', 'rect')
+      .attr('transform', `translate(${padding.left},${padding.top})`)
+      .attr('x', (d, i) => xScale(i) + 20/2)
+      .attr('y', d => yScale(d))
+      .attr('width', 20)
+      .attr('height', d => height - yScale(d) - padding.bottom - padding.top)
+      .attr('fill', 'steelblue')
+
+    console.log(xAxis)
+    // svg.append('g')
+    //   .selectAll('line')
+    //   .data(data)
+    //   .enter()
+    //   .append('line')
+    //   .attr('x2', width - padding.left - padding.right)
+    //   .attr('stroke', '#000')
+    //   .attr('transform', (d, i) => `translate(${padding.left}, ${yScale(d) + padding.top})`)
+
+    let x = svg.append('g')
+    .attr('transform', `translate(${padding.left}, ${height - padding.top})`)
+    .call(xAxis)
+
+    x.select('path')
+    .attr('stroke', '#E8E8E8')
+
+    x.selectAll('line')
+    .attr('y2', -6)
+
 
 
   }
@@ -157,7 +203,7 @@ class DataV extends React.Component {
     const { colorArr, nameArr } = this
     console.log(nameArr)
     return (
-      <div>
+      <div className={style.flex}>
         <div className={style.container}>
           <div className={style.legend}>
             <ul>
@@ -171,6 +217,7 @@ class DataV extends React.Component {
           <div id="svg"></div>
         </div>
         <svg id="pie"></svg>
+        <svg id="bar"></svg>
       </div>
     )
   }
